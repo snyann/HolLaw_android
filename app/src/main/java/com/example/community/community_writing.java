7f8d92393960135bde36df6095789d2a2723c3d1
윤승현 <hyeondc99>
2022-07-28 17:18:43 +0900

    community_writing.java에 delete 버튼 클릭시 커뮤니티 초기화면으로 이동하도록하는 이벤트 생성

--- app/src/main/java/com/example/community/community_writing.java
+++ app/src/main/java/com/example/community/community_writing.java
@@ -1,4 +1,21 @@
 package com.example.community;
 
-public class community_writing {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class community_writing extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.community_writing);
+        ImageView iv_delete=(ImageView) findViewById(R.id.iv_delete);
+        iv_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
