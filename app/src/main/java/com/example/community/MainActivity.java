6f6fb42e455b20bb16fc5d387166cea466554532
윤승현 <hyeondc99>
2022-07-29 18:57:17 +0900

    MainActivity(커뮤니티 초기화면)에서 검색아이콘 클릭 시 내용리스트 레이아웃으로, 글쓰기버튼 클릭시 게시글 작성 레이아웃으로 전환되는 이벤트 생성

--- app/src/main/java/com/example/community/MainActivity.java
+++ app/src/main/java/com/example/community/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.community;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.community);
+        ImageButton ib_writing = (ImageButton) findViewById(R.id.ib_writing);
+        ib_writing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), community_writing.class);
+                startActivity(intent);
+            }
+        });
+        ImageButton ib_community_search = (ImageButton) findViewById(R.id.ib_community_search);
+        ib_community_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), community_content_list.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/community/community_content_list.java
@@ -0,0 +1,23 @@
+package com.example.community;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+
+public class community_content_list extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.community_content_list);
+        ImageButton ib_back=(ImageButton) findViewById(R.id.ib_back);
+        ib_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
