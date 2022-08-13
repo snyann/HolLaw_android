764f5dd49c54ccacbfdc68abb0deda17b4217af4
jeongmin1221 <jeong7160@naver.com>
2022-08-14 04:15:19 +0900

    메인엑티비티 -> 커뮤니티 액티비티로 코드 이동

--- app/src/main/java/com/example/community/MainActivity.java
+++ app/src/main/java/com/example/community/MainActivity.java
@@ -1,41 +1,20 @@
 package com.example.community;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-<<<<<<< HEAD
 import android.os.Bundle;
-=======
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
->>>>>>> branch_sh
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
+
         setContentView(R.layout.activity_main);
-=======
-        setContentView(R.layout.community);
-        ImageButton ib_writing = (ImageButton) findViewById(R.id.ib_writing);
-        ib_writing.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), community_writing.class);
-                startActivity(intent);
-            }
-        });
-        ImageButton ib_community_search = (ImageButton) findViewById(R.id.ib_community_search);
-        ib_community_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), community_content_list.class);
-                startActivity(intent);
-            }
-        });
->>>>>>> branch_sh
+
     }
 }
