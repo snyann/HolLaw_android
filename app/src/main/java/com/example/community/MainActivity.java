285d962d122ac12f8893493ae1f60fff8311e081
jeongmin1221 <jeong7160@naver.com>
2022-08-15 02:31:09 +0900

    LoginActivity 버튼 연결

--- app/src/main/java/com/example/community/MainActivity.java
+++ app/src/main/java/com/example/community/MainActivity.java
@@ -1,19 +1,37 @@
 package com.example.community;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private FragmentManager fragmentManager;
+    private Button btn_mypage, btn_register;
+    private EditText et_id, et_password;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton btn_mypage = findViewById(R.id.btn_mypage);
+        btn_mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
