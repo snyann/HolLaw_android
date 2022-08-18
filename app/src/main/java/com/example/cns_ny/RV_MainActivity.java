bfce271b430a83fff6dd9c64f20d718f1dbaa8e7
김나연 <slvx0524@gmail.com>
2022-08-18 18:18:26 +0900

    re_revision_law.xml 리싸이클러뷰 설정

--- app/src/main/java/com/example/cns_ny/RV_MainActivity.java
+++ app/src/main/java/com/example/cns_ny/RV_MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.cns_ny;
 
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.community.R;
+
 import java.util.Arrays;
 import java.util.List;
 
