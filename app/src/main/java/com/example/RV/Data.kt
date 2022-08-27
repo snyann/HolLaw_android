15e5ea03f6df509d69a6a8b0e3732a44441af7dd
김나연 <slvx0524@gmail.com>
2022-08-27 15:37:00 +0900

    리사이클러뷰 검색결과 적용

--- app/src/main/java/com/example/RV/Data.kt
+++ app/src/main/java/com/example/RV/Data.kt
@@ -1,17 +1,6 @@
 package com.example.RV
 
-import androidx.recyclerview.widget.RecyclerView
-import com.example.RV.RecyclerAdapter.ItemViewHolder
-import android.view.ViewGroup
-import android.view.LayoutInflater
-import com.example.community.R
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-
-class Data {
-    var title: String? = null
-    var content: String? = null
-    private val iv_comment = 0
-}
+data class Data (
+    val title: String,
+    val content: String
+)
--- app/src/main/java/com/example/community/Activity/ResultActivity.kt
+++ app/src/main/java/com/example/community/Activity/ResultActivity.kt
@@ -2,11 +2,24 @@ package com.example.community.Activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.community.R
+import com.example.community.LawAdapter
+import com.example.community.LawList
+import com.example.community.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    var binding: ActivityResultBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        val adapter = LawAdapter(LawList.lawList)
+        binding?.recyclerView?.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
