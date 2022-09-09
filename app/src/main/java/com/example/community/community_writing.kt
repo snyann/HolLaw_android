95c041a347b8f73e2093235526cbf14f3c01bbc6
jeong716234 <jeong716234@gmail.com>
2022-09-09 14:44:34 +0900

    아이콘 수정

--- app/src/main/java/com/example/community/community_writing.kt
+++ app/src/main/java/com/example/community/community_writing.kt
@@ -3,13 +3,12 @@ package com.example.community
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
-import com.example.community.R
 import android.widget.ImageButton
 
 class community_writing : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.community_writing)
+        setContentView(R.layout.activity_writing)
         val ib_delete = findViewById<View>(R.id.btn_backspace) as ImageButton
         ib_delete.setOnClickListener { finish() }
     }
