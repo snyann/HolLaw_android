e1d639cdda40574d2d4bc33ebae197554295e7f0
jeong716234 <jeong716234@gmail.com>
2022-10-02 07:46:50 +0900

    getPost 수정 게시물 받아오는 거

--- app/src/main/java/com/example/community/Activity/CommunityActivity.kt
+++ app/src/main/java/com/example/community/Activity/CommunityActivity.kt
@@ -1,7 +1,10 @@
 package com.example.community.Activity
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.RV.PostInfo
 import com.example.adapters.CommunityAdapter
@@ -10,13 +13,14 @@ import com.example.models.CommunityList
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
+
 class CommunityActivity : AppCompatActivity() {
 
     private lateinit var fbAuth: FirebaseAuth
     lateinit var binding: ActivityCommunityBinding
     //private var itemList = arrayListOf<CommunityList>() //리스트 아이템 배열
-    private lateinit var fbdb:DatabaseReference
-
+    private lateinit var fbdb:FirebaseDatabase
+    private lateinit var ref : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,7 @@ class CommunityActivity : AppCompatActivity() {
         binding = ActivityCommunityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //게시물 작성으로 이동
         binding.ibWriting.setOnClickListener{
             val intent = Intent(this, WritingActivity::class.java)
             intent.putExtra("uid",fbAuth.currentUser?.uid)
@@ -31,13 +36,15 @@ class CommunityActivity : AppCompatActivity() {
         }
 
 
-        fbdb = FirebaseDatabase.getInstance().getReference().child("uid")
+        fbdb = FirebaseDatabase.getInstance()
         fbAuth = FirebaseAuth.getInstance()
 
         //리스트 업데이트
         val adapter = CommunityAdapter(CommunityList.communityList)
         binding!!.recyclerView2?.adapter = adapter
 
+        /*
+        .getReference().child("uid")
         fbdb.addValueEventListener(object : ValueEventListener{
             override fun onCancelled(dataSnapshot: DatabaseError) {
             }
@@ -51,8 +58,28 @@ class CommunityActivity : AppCompatActivity() {
 
             }
         })
+*/      adapter
+        getPost()
 
-        adapter.notifyDataSetChanged()
+    }
+
+    private fun getPost() {
+        fbdb=FirebaseDatabase.getInstance()
+        ref = fbdb.getReference("/uid/postinfo")
+
+
+        ref.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        val childEventListener = object : ChildEventListener()
+        ref.addChildEventListener(childEventListener)
 
 
     }
